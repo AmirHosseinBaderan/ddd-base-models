365f7de4359a450a7bbe2fb03646142be56e30fd
amirhossein <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-09-21 14:30:51 +0330

    update : config events

--- DDD.BaseModels/Src/DDD.BaseModels/Config.cs
+++ DDD.BaseModels/Src/DDD.BaseModels/Config.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using DDD.BaseModels.Service;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +11,31 @@ public static class DddConfig
         services.AddScoped(typeof(IBaseCud<,>), typeof(BaseCud<,>));
         services.AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
 
+        return services;
+    }
+
+    public static void ConfigDDDEvents(this IServiceCollection services,Assembly  assembly)
+    {
+        // Base
+        services.AddSingleton<IDomainEventDispatcher, DomainEventDispatcher>();
+
+        // Config events
+        services.AddDomainEventHandlers(assembly);
+    }
+
+    static IServiceCollection AddDomainEventHandlers(this IServiceCollection services, Assembly assembly)
+    {
+        // Find all closed generic implementations of IDomainEventHandler<T>
+        var handlers = assembly.GetTypes()
+            .Where(t => t is { IsAbstract: false, IsInterface: false })
+            .SelectMany(t => t.GetInterfaces(), (t, i) => new { Handler = t, Interface = i })
+            .Where(x =>
+                x.Interface.IsGenericType &&
+                x.Interface.GetGenericTypeDefinition() == typeof(IDomainEventHandler<>))
+            .ToList();
+
+        foreach (var h in handlers)
+            services.AddScoped(h.Interface, h.Handler);
         return services;
     }
 }
