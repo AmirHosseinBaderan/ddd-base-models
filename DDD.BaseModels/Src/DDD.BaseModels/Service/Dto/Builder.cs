01917bda3232921f77f9a8ee55f6bed021d2fd9d
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-09-26 16:02:14 +0330

    update : add type builder

--- DDD.BaseModels/Src/DDD.BaseModels/Service/Dto/Builder.cs
+++ DDD.BaseModels/Src/DDD.BaseModels/Service/Dto/Builder.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using System.Reflection.Emit;
+
 namespace DDD.BaseModels.Service;
 
 public class DtoBuilder<T>(T aggregate)
@@ -57,6 +60,40 @@ public class DtoBuilder<T>(T aggregate)
         return props;
     }
 
+    // Build runtime type for Swagger
+    public Type BuildType(string typeName = "DynamicDto")
+    {
+        var assemblyName = new AssemblyName("DynamicDtos");
+        var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+        var moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
+
+        var typeBuilder = moduleBuilder.DefineType(
+            typeName,
+            TypeAttributes.Public | TypeAttributes.Class);
+
+        // Always include Id
+        typeBuilder.DefinePropertyWithBackingField("Id", typeof(Guid));
+
+        // Aggregate props
+        var props = typeof(T).GetProperties()
+            .Where(p => p.CanRead
+                        && p.Name != nameof(AggregateRootBase.Id)
+                        && !_ignoredProps.Contains(p.Name));
+
+        foreach (var p in props)
+        {
+            typeBuilder.DefinePropertyWithBackingField(p.Name, p.PropertyType);
+        }
+
+        // Extra props (always object type at runtime)
+        foreach (var (key, _) in _extraProps)
+        {
+            typeBuilder.DefinePropertyWithBackingField(key, typeof(object));
+        }
+
+        return typeBuilder.CreateType()!;
+    }
+
     private static string GetPropertyName(Expression<Func<T, object>> expression)
     {
         if (expression.Body is MemberExpression member)
@@ -66,5 +103,40 @@ public class DtoBuilder<T>(T aggregate)
             return member2.Member.Name;
 
         throw new ArgumentException("Invalid expression");
+    }
+}
+
+// helper extension for generating properties
+public static class TypeBuilderExtensions
+{
+    public static void DefinePropertyWithBackingField(this TypeBuilder typeBuilder, string propertyName, Type propertyType)
+    {
+        var fieldBuilder = typeBuilder.DefineField($"_{propertyName}", propertyType, FieldAttributes.Private);
+
+        var propBuilder = typeBuilder.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null);
+
+        // Getter
+        var getter = typeBuilder.DefineMethod($"get_{propertyName}",
+            MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig,
+            propertyType, Type.EmptyTypes);
+
+        var getterIL = getter.GetILGenerator();
+        getterIL.Emit(OpCodes.Ldarg_0);
+        getterIL.Emit(OpCodes.Ldfld, fieldBuilder);
+        getterIL.Emit(OpCodes.Ret);
+
+        // Setter
+        var setter = typeBuilder.DefineMethod($"set_{propertyName}",
+            MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig,
+            null, new[] { propertyType });
+
+        var setterIL = setter.GetILGenerator();
+        setterIL.Emit(OpCodes.Ldarg_0);
+        setterIL.Emit(OpCodes.Ldarg_1);
+        setterIL.Emit(OpCodes.Stfld, fieldBuilder);
+        setterIL.Emit(OpCodes.Ret);
+
+        propBuilder.SetGetMethod(getter);
+        propBuilder.SetSetMethod(setter);
     }
 }
--- DDD.BaseModels/Src/Test/Dto/DtoTest.cs
+++ DDD.BaseModels/Src/Test/Dto/DtoTest.cs
@@ -38,6 +38,59 @@ public class DtoTest(ITestOutputHelper outputHelper)
         outputHelper.WriteLine("Input model is {0} and Dto is {1}", JsonConvert.SerializeObject(domain),
             JsonConvert.SerializeObject(dto));
     }
+    
+    [Fact]
+    public void TestDtoTypeBuilder()
+    {
+        DateTime now = DateTime.Now;
+        Domain domain = new()
+        {
+            Id = EntityId.CreateUniqueId(),
+            Name = "amir",
+            LastName = "baderan",
+            CreatedOn = now,
+        };
+
+        // Build dynamic DTO type
+        var dtoType = domain
+            .ToDtoBuilder()
+            .Ignore(x => x.Events)
+            .IgnoreWhere(x => x.UpdatedOn, x => x.UpdatedOn == null)
+            .Add("TestAdd", x => "value test")
+            .BuildType("DomainDto");
+
+        // Create instance of the dynamic type
+        var dtoInstance = Activator.CreateInstance(dtoType)!;
+
+        // Copy properties from aggregate to DTO
+        foreach (var prop in dtoType.GetProperties())
+        {
+            object? value = null;
+
+            if (prop.Name == "Id")
+                value = domain.Id.Value;
+            else if (prop.Name == "Name")
+                value = domain.Name;
+            else if (prop.Name == "LastName")
+                value = domain.LastName;
+            else if (prop.Name == "CreatedOn")
+                value = domain.CreatedOn;
+            else if (prop.Name == "TestAdd")
+                value = "value test";
+
+            prop.SetValue(dtoInstance, value);
+        }
+
+        // Serialize input and output for verification
+        outputHelper.WriteLine("Input model: {0}", JsonConvert.SerializeObject(domain));
+        outputHelper.WriteLine("Dto object : {0}", JsonConvert.SerializeObject(dtoInstance));
+
+        // Assert basic checks
+        var dtoJson = JsonConvert.SerializeObject(dtoInstance);
+        Assert.Contains("amir", dtoJson);
+        Assert.Contains("value test", dtoJson);
+        Assert.DoesNotContain("Events", dtoJson); // Ignored
+    }
 }
 
 class Domain : AggregateRootBase
